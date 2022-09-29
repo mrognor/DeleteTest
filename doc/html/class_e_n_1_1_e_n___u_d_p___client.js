8bf3d951641290d8cd511f703ea9caf00afea4bf
Timur Ammaev <timurlego1@yandex.ru>
2022-09-29 12:11:11 +0300

    Update docs

--- doc/html/class_e_n_1_1_e_n___u_d_p___client.js
+++ doc/html/class_e_n_1_1_e_n___u_d_p___client.js
@@ -1,12 +1,12 @@
 var class_e_n_1_1_e_n___u_d_p___client =
 [
-    [ "Close", "class_e_n_1_1_e_n___u_d_p___client.html#a4580def64540c7156f606832ba0c4b70", null ],
     [ "GetIpAddr", "class_e_n_1_1_e_n___u_d_p___client.html#a297b8d26f3965a2ecc1609ae62a7364e", null ],
     [ "GetPort", "class_e_n_1_1_e_n___u_d_p___client.html#a1e8589d4b8a8af5a2db313a65be30bfb", null ],
     [ "GetSocket", "class_e_n_1_1_e_n___u_d_p___client.html#a45c6e4da9bb1a31c42bbf2da93d9a04e", null ],
     [ "Run", "class_e_n_1_1_e_n___u_d_p___client.html#a4751eab3ad696f6f740cbb21434b54ed", null ],
     [ "SendToServer", "class_e_n_1_1_e_n___u_d_p___client.html#a95b11e4a64358af9ffe7287eac17f279", null ],
     [ "ServerMessageHandler", "class_e_n_1_1_e_n___u_d_p___client.html#aec18f98cbdc95f77816f070bba6b3afb", null ],
+    [ "Stop", "class_e_n_1_1_e_n___u_d_p___client.html#a1a57fa02dc7f7ef24476b76bd054b146", null ],
     [ "MaxMessageSize", "class_e_n_1_1_e_n___u_d_p___client.html#ad234ba02ece2d965b157e9100c8639d9", null ],
     [ "ServerIpAddress", "class_e_n_1_1_e_n___u_d_p___client.html#aba2523ac99417083f2ee957d5cf0a5eb", null ],
     [ "ServerPort", "class_e_n_1_1_e_n___u_d_p___client.html#ae259275e8195c9de6e0e8d75b00e4b23", null ]
